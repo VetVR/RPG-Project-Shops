db6e4370e2a9f552be2ad6b9d9f76be7d3bc589b
Ron H <ronhat@gmail.com>
2022-06-22 12:54:35 -0700

    Added Windmill

---
+++ Assets/WindMill/Scripts/Rotarion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotarion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var myRotation = Quaternion.Euler(0, 0, 1); 
+		transform.rotation *= myRotation; 
+	}
+}
